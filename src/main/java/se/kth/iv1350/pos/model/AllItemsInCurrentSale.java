64219ee60fd856f6329e47d69c8f8ce9affba9ea
Noxiium <johansf@hotmail.com>
2022-05-03 17:21:28 +0200

    addItem method/test

--- src/main/java/se/kth/iv1350/pos/model/AllItemsInCurrentSale.java
+++
@@ -1,9 +0,0 @@
-package se.kth.iv1350.pos.model;
-
-import java.util.ArrayList;
-import se.kth.iv1350.pos.integration.ItemDTO;
-
-public class AllItemsInCurrentSale {
-    private ArrayList<ItemDTO> itemsInSale = new ArrayList<>();
-    
-}
