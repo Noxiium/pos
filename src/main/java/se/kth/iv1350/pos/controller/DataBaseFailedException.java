126aa7d6dc67568d82cb9fcc4d9f51a059ca8f90
Noxiium <90914884+Noxiium@users.noreply.github.com>
2022-05-20 13:59:21 +0200

    Update DataBaseFailedException.java
    Added comments

--- src/main/java/se/kth/iv1350/pos/controller/DataBaseFailedException.java
+++ src/main/java/se/kth/iv1350/pos/controller/DataBaseFailedException.java
@@ -1,10 +1,13 @@
 package se.kth.iv1350.pos.controller;
 
 /**
- *
+ * Throws when the controller can't connect to the Inventory System.
  */
 public class DataBaseFailedException extends Exception {
     
+    /**
+     * Creates a new instance
+     */
     public DataBaseFailedException(){
         super("Can't connect to external systems. Try again");
     }
